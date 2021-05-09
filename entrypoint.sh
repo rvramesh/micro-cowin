fea0dc29f78449f639e861a3efd871058ef7f574
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2021-05-09 08:59:23 +0530

    Removed rest entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# change ownership to user=nobody, group=nobody
-sudo chown -R appuser:appuser /data
-
-# open with su a new shell as user nobody and call your applic
-su -s /bin/bash -c "dotnet /app/publish/micro-win.dll" appuser
-
